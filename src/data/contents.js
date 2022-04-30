beac5e4d9547bb28d0c07c7bb5f120c0a1601686
cnt000 <cnt000@gmail.com>
2022-04-30 21:06:00 +0200

    some content

--- src/data/contents.js
+++ src/data/contents.js
@@ -3,10 +3,76 @@ export const contents = [
     title: "Work Experience",
     content: [
       {
-        title: "Full-stack senior developer",
+        title: "Full-stack developer (Consultant)",
+        duration: "08/2021 - today",
+        link: "https://www.paramount.com",
+        company: "Paramount (Full-remote)",
+        text: `TODO`,
+
+      },
+      {
+        title: "Full-stack developer",
         duration: "07/2020 - 10/2021",
         link: "https://www.monrif.it",
         company: "MONRIF (Bologna)",
+        text: `I'm a full-stack developer with a deep knowledge of frontend and JavaScript.
+
+I work on projects using Js ecosystem introducing JavaScript in the company.
+Monrif needs an experienced JavaScript developer for the client and for the server side.
+We are a highly skilled 5 people team, covering all positions from devops to backend/frontend.
+I also do mentoring by facilitating colleagues to switch from python to JavaScript, and to improve the agile practices used.
+
+As a Senior developer, here's what I do:
+
+- Learn something new, everyday
+- Work on large-scale, custom-designed, enterprise-level software development projects using JavaScript language and technologies.
+- Champion agile best practices and Continuous Delivery
+- Help the team to champion software excellence, craft technical vision and ensure we deliver value to our clients and their customers.
+- Experience working with agile, Lean and/or Continuous Delivery approaches and best practices, such as Continuous Integration, TDD, BDD, etc.
+- Knowledge of software best practices like Refactoring, Clean Code, Domain Driven Design, Encapsulation.
+- Openness and adaptability - you’ll be working in a variety of settings and in a team-oriented, collaborative environment
+- A coaching mentality as well as strong relationship-building skills at all levels
+- All these things made with passion.
+- Databases like MySQL, Oracle, PostgreSQL, SQL Server, NoSQL.
+
+Websites:
+
+- https://www.ilrestodelcarlino.it/
+- https://www.ilgiorno.it/
+- https://www.lanazione.it/
+- https://www.quotidiano.net/
+
+## Key responsibilities
+
+- Leading projects #LEAD
+- Developing with quality #PROGRAMMING
+- Developing testing strategies #TESTING
+- Defining best practices to keep high software performances #PERFORMANCE
+- Designing the evolutions of software products #EVOLUTION
+- Refactor old tools and services #REFACTOR
+- Leading planning activities and supporting in predictability #AGILE
+- Defining and steering technical strategy with technical leads and architects#STRATEGY
+- Supporting Project Managers #SUPPORT
+- Team coaching and mentoring through workshops #MENTORING
+
+## Technologies
+
+- HTML5, CSS3, JS
+
+- React, Vanilla Js, Webpack, Node.js, Python
+
+- NoSQL DB, Relational DB (Oracle, MySQL, Microsoft SQL)
+
+- Strapi, Ghost, Stripe, Sanity, Gatsby, Next.js
+
+- Git, Agile, Bash, AWS, Google Cloud`,
+
+      },
+      {
+        title: "Tech lead - front-end team",
+        duration: "03/2013 - 06/2020",
+        link: "https://www.ynap.com",
+        company: "YOOX NET-A-PORTER (Bologna)",
         text: `My daily job is relative to team management, programming, planning, analysis of new functionalities and website performance enhancement, ensuring technical cohesion across the domain teams and acting as the technical domain representative in cross-domain matters.
 
 My team is in charge of the following three luxury e-commerce websites, with focus on the development of new features, integration and refactorization of existing features, measuring and optimizing the website performances:
@@ -37,7 +103,6 @@ My team is in charge of the following three luxury e-commerce websites, with foc
 - Supporting Project Managers and Product Managers #SUPPORT
 
 - Team coaching and mentoring through workshops and communities #MENTORING
-
 
 ## Technologies
 
@@ -54,157 +119,35 @@ My team is in charge of the following three luxury e-commerce websites, with foc
 - Agile`,
       },
       {
-        title: "Full-stack senior developer",
-        duration: "07/2020 - 10/2021",
-        link: "https://www.monrif.it",
-        company: "MONRIF (Bologna)",
-        text: `My daily job is relative to team management, programming, planning, analysis of new functionalities and website performance enhancement, ensuring technical cohesion across the domain teams and acting as the technical domain representative in cross-domain matters.
-
-My team is in charge of the following three luxury e-commerce websites, with focus on the development of new features, integration and refactorization of existing features, measuring and optimizing the website performances:
-
-- [balenciaga.com](https://www.balenciaga.com)
-
-- [alexandermcqueen.com](https://www.alexandermcqueen.com])
-
-- [mcq.com](https://www.mcq.com)
-
+        title: "Front-end Developer (Consultant)",
+        duration: "04/2011 - 03/2013",
+        link: "https://www.ynap.com",
+        company: "YOOX NET-A-PORTER (Bologna)",
+        text: `As consultant, I was in charge of the development and maintenance of e-commerce websites powered by YOOX NET-A-PORTER GROUP.
 
 ## Key responsibilities
 
-- Leading a front-end development team #LEAD
-
-- Developing in a number of programming languages, with quality #PROGRAMMING
-
-- Developing testing strategies #TESTING
-
-- Defining best practices to keep high software performances #PERFORMANCE
-
-- Designing the evolutions of software products #EVOLUTION
-
-- Leading planning activities and supporting in predictability #AGILE
-
-- Defining and steering technical strategy with technical leads and architects #STRATEGY
-
-- Supporting Project Managers and Product Managers #SUPPORT
-
-- Team coaching and mentoring through workshops and communities #MENTORING
-
+- Restyling of existing websites and adding new features and functionalities
+- Front-End optimization (pixel perfection from layouts,…) and mapping and skinning functionalities from backend side
+- Bug fixing
+- Web performance optimization
 
 ## Technologies
 
-- HTML5, CSS3, JS
-
-- React, Vue.js and relative ecosystem
-
-- NoSQL DB, Relational DB (Oracle, MySQL, Microsoft SQL)
-
-- C#, RAZOR, MVC framework
-
-- Website performance optimization
-
-- Agile`,
+- ASP, HTML5, CSS (CSS3), JavaScript, XSL, MVC Razor, Agile`,
       },
       {
-        title: "Full-stack senior developer",
-        duration: "07/2020 - 10/2021",
-        link: "https://www.monrif.it",
-        company: "MONRIF (Bologna)",
-        text: `My daily job is relative to team management, programming, planning, analysis of new functionalities and website performance enhancement, ensuring technical cohesion across the domain teams and acting as the technical domain representative in cross-domain matters.
+        title: "PHP Developer and Web developer",
+        duration: "01/2009 - 03/2011",
+        link: "https://www.cineca.it",
+        company: "CINECA (Bologna)",
+        text: `I started my professional career developing web applications working with PHP and Oracle Databases.
 
-My team is in charge of the following three luxury e-commerce websites, with focus on the development of new features, integration and refactorization of existing features, measuring and optimizing the website performances:
+## Main activities
 
-- [balenciaga.com](https://www.balenciaga.com)
-
-- [alexandermcqueen.com](https://www.alexandermcqueen.com])
-
-- [mcq.com](https://www.mcq.com)
-
-
-## Key responsibilities
-
-- Leading a front-end development team #LEAD
-
-- Developing in a number of programming languages, with quality #PROGRAMMING
-
-- Developing testing strategies #TESTING
-
-- Defining best practices to keep high software performances #PERFORMANCE
-
-- Designing the evolutions of software products #EVOLUTION
-
-- Leading planning activities and supporting in predictability #AGILE
-
-- Defining and steering technical strategy with technical leads and architects #STRATEGY
-
-- Supporting Project Managers and Product Managers #SUPPORT
-
-- Team coaching and mentoring through workshops and communities #MENTORING
-
-
-## Technologies
-
-- HTML5, CSS3, JS
-
-- React, Vue.js and relative ecosystem
-
-- NoSQL DB, Relational DB (Oracle, MySQL, Microsoft SQL)
-
-- C#, RAZOR, MVC framework
-
-- Website performance optimization
-
-- Agile`,
-      },
-      {
-        title: "Full-stack senior developer",
-        duration: "07/2020 - 10/2021",
-        link: "https://www.monrif.it",
-        company: "MONRIF (Bologna)",
-        text: `My daily job is relative to team management, programming, planning, analysis of new functionalities and website performance enhancement, ensuring technical cohesion across the domain teams and acting as the technical domain representative in cross-domain matters.
-
-My team is in charge of the following three luxury e-commerce websites, with focus on the development of new features, integration and refactorization of existing features, measuring and optimizing the website performances:
-
-- [balenciaga.com](https://www.balenciaga.com)
-
-- [alexandermcqueen.com](https://www.alexandermcqueen.com])
-
-- [mcq.com](https://www.mcq.com)
-
-
-## Key responsibilities
-
-- Leading a front-end development team #LEAD
-
-- Developing in a number of programming languages, with quality #PROGRAMMING
-
-- Developing testing strategies #TESTING
-
-- Defining best practices to keep high software performances #PERFORMANCE
-
-- Designing the evolutions of software products #EVOLUTION
-
-- Leading planning activities and supporting in predictability #AGILE
-
-- Defining and steering technical strategy with technical leads and architects #STRATEGY
-
-- Supporting Project Managers and Product Managers #SUPPORT
-
-- Team coaching and mentoring through workshops and communities #MENTORING
-
-
-## Technologies
-
-- HTML5, CSS3, JS
-
-- React, Vue.js and relative ecosystem
-
-- NoSQL DB, Relational DB (Oracle, MySQL, Microsoft SQL)
-
-- C#, RAZOR, MVC framework
-
-- Website performance optimization
-
-- Agile`,
+- Development of web applications for clinical trials and pharmaceutical procedures
+- Creation and maintenance of the internal Help Desk application
+- Development of corporate websites in Drupal and Wordpress`,
       },
     ],
   },
@@ -212,15 +155,14 @@ My team is in charge of the following three luxury e-commerce websites, with foc
     title: "Volunteer",
     content: [
       {
-        title: "Full-stack senior developer",
-        duration: "",
-        link: "",
-        company: "",
-        text: `Organiser Bologna JavaScript Community/Meetup2015 - Today
-        http://bolognajs.com/
-        BolognaJS is a community for JavaScript developers in Bologna. We made workshops, talks, networking meetings, we believe in sharing knowledge and we study new technologies on JavaScript ecosystem. I love to meet people and to speak about Programming feature, technique and do something together.
+        title: "Organiser",
+        duration: "2005 - today",
+        link: "http://bolognajs.com/",
+        company: "Bologna JavaScript Community/Meetup",
+        text: `Organiser Bologna JavaScript Community/Meetup
+BolognaJS is a community for JavaScript developers in Bologna. We made workshops, talks, networking meetings, we believe in sharing knowledge and we study new technologies on JavaScript ecosystem. I love to meet people and to speak about Programming feature, technique and do something together.
         
-        See our last meetup on: https://www.meetup.com/Bologna-JS-Meetup/`,
+See our last meetup on: https://www.meetup.com/Bologna-JS-Meetup/`,
       },
     ],
   },
@@ -228,13 +170,19 @@ My team is in charge of the following three luxury e-commerce websites, with foc
     title: "Education",
     content: [
       {
-        title: "Full-stack senior developer",
-        duration: "",
-        link: "",
-        company: "",
-        text: `Master's degree in Computer Science University of Bologna (UniBo)2005 - 2008
-        Bachelor’s degree in Computer Science University of Bologna (UniBo)2001 - 2005`,
+        title: "Master's degree in Computer Science",
+        duration: "2005 - 2008",
+        link: "http://www.unibo.it/",
+        company: "University of Bologna (UniBo)",
+        text: ``,
+      },
+      {
+        title: "Bachelor's degree in Computer Science",
+        duration: "2001 - 2005",
+        link: "http://www.unibo.it/",
+        company: "University of Bologna (UniBo)",
+        text: ``,
       },
     ],
-  }
+  },
 ];
