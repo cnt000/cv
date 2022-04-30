51c3ccca99ee7b11f079a27a46c0a8d778e85635
cnt000 <cnt000@gmail.com>
2022-04-30 15:50:36 +0200

    experiences

---
+++ src/data/contents.js
@@ -0,0 +1,240 @@
+export const contents = [
+  {
+    title: "Work Experience",
+    content: [
+      {
+        title: "Full-stack senior developer",
+        duration: "07/2020 - 10/2021",
+        link: "https://www.monrif.it",
+        company: "MONRIF (Bologna)",
+        text: `My daily job is relative to team management, programming, planning, analysis of new functionalities and website performance enhancement, ensuring technical cohesion across the domain teams and acting as the technical domain representative in cross-domain matters.
+
+My team is in charge of the following three luxury e-commerce websites, with focus on the development of new features, integration and refactorization of existing features, measuring and optimizing the website performances:
+
+- [balenciaga.com](https://www.balenciaga.com)
+
+- [alexandermcqueen.com](https://www.alexandermcqueen.com])
+
+- [mcq.com](https://www.mcq.com)
+
+
+## Key responsibilities
+
+- Leading a front-end development team #LEAD
+
+- Developing in a number of programming languages, with quality #PROGRAMMING
+
+- Developing testing strategies #TESTING
+
+- Defining best practices to keep high software performances #PERFORMANCE
+
+- Designing the evolutions of software products #EVOLUTION
+
+- Leading planning activities and supporting in predictability #AGILE
+
+- Defining and steering technical strategy with technical leads and architects #STRATEGY
+
+- Supporting Project Managers and Product Managers #SUPPORT
+
+- Team coaching and mentoring through workshops and communities #MENTORING
+
+
+## Technologies
+
+- HTML5, CSS3, JS
+
+- React, Vue.js and relative ecosystem
+
+- NoSQL DB, Relational DB (Oracle, MySQL, Microsoft SQL)
+
+- C#, RAZOR, MVC framework
+
+- Website performance optimization
+
+- Agile`,
+      },
+      {
+        title: "Full-stack senior developer",
+        duration: "07/2020 - 10/2021",
+        link: "https://www.monrif.it",
+        company: "MONRIF (Bologna)",
+        text: `My daily job is relative to team management, programming, planning, analysis of new functionalities and website performance enhancement, ensuring technical cohesion across the domain teams and acting as the technical domain representative in cross-domain matters.
+
+My team is in charge of the following three luxury e-commerce websites, with focus on the development of new features, integration and refactorization of existing features, measuring and optimizing the website performances:
+
+- [balenciaga.com](https://www.balenciaga.com)
+
+- [alexandermcqueen.com](https://www.alexandermcqueen.com])
+
+- [mcq.com](https://www.mcq.com)
+
+
+## Key responsibilities
+
+- Leading a front-end development team #LEAD
+
+- Developing in a number of programming languages, with quality #PROGRAMMING
+
+- Developing testing strategies #TESTING
+
+- Defining best practices to keep high software performances #PERFORMANCE
+
+- Designing the evolutions of software products #EVOLUTION
+
+- Leading planning activities and supporting in predictability #AGILE
+
+- Defining and steering technical strategy with technical leads and architects #STRATEGY
+
+- Supporting Project Managers and Product Managers #SUPPORT
+
+- Team coaching and mentoring through workshops and communities #MENTORING
+
+
+## Technologies
+
+- HTML5, CSS3, JS
+
+- React, Vue.js and relative ecosystem
+
+- NoSQL DB, Relational DB (Oracle, MySQL, Microsoft SQL)
+
+- C#, RAZOR, MVC framework
+
+- Website performance optimization
+
+- Agile`,
+      },
+      {
+        title: "Full-stack senior developer",
+        duration: "07/2020 - 10/2021",
+        link: "https://www.monrif.it",
+        company: "MONRIF (Bologna)",
+        text: `My daily job is relative to team management, programming, planning, analysis of new functionalities and website performance enhancement, ensuring technical cohesion across the domain teams and acting as the technical domain representative in cross-domain matters.
+
+My team is in charge of the following three luxury e-commerce websites, with focus on the development of new features, integration and refactorization of existing features, measuring and optimizing the website performances:
+
+- [balenciaga.com](https://www.balenciaga.com)
+
+- [alexandermcqueen.com](https://www.alexandermcqueen.com])
+
+- [mcq.com](https://www.mcq.com)
+
+
+## Key responsibilities
+
+- Leading a front-end development team #LEAD
+
+- Developing in a number of programming languages, with quality #PROGRAMMING
+
+- Developing testing strategies #TESTING
+
+- Defining best practices to keep high software performances #PERFORMANCE
+
+- Designing the evolutions of software products #EVOLUTION
+
+- Leading planning activities and supporting in predictability #AGILE
+
+- Defining and steering technical strategy with technical leads and architects #STRATEGY
+
+- Supporting Project Managers and Product Managers #SUPPORT
+
+- Team coaching and mentoring through workshops and communities #MENTORING
+
+
+## Technologies
+
+- HTML5, CSS3, JS
+
+- React, Vue.js and relative ecosystem
+
+- NoSQL DB, Relational DB (Oracle, MySQL, Microsoft SQL)
+
+- C#, RAZOR, MVC framework
+
+- Website performance optimization
+
+- Agile`,
+      },
+      {
+        title: "Full-stack senior developer",
+        duration: "07/2020 - 10/2021",
+        link: "https://www.monrif.it",
+        company: "MONRIF (Bologna)",
+        text: `My daily job is relative to team management, programming, planning, analysis of new functionalities and website performance enhancement, ensuring technical cohesion across the domain teams and acting as the technical domain representative in cross-domain matters.
+
+My team is in charge of the following three luxury e-commerce websites, with focus on the development of new features, integration and refactorization of existing features, measuring and optimizing the website performances:
+
+- [balenciaga.com](https://www.balenciaga.com)
+
+- [alexandermcqueen.com](https://www.alexandermcqueen.com])
+
+- [mcq.com](https://www.mcq.com)
+
+
+## Key responsibilities
+
+- Leading a front-end development team #LEAD
+
+- Developing in a number of programming languages, with quality #PROGRAMMING
+
+- Developing testing strategies #TESTING
+
+- Defining best practices to keep high software performances #PERFORMANCE
+
+- Designing the evolutions of software products #EVOLUTION
+
+- Leading planning activities and supporting in predictability #AGILE
+
+- Defining and steering technical strategy with technical leads and architects #STRATEGY
+
+- Supporting Project Managers and Product Managers #SUPPORT
+
+- Team coaching and mentoring through workshops and communities #MENTORING
+
+
+## Technologies
+
+- HTML5, CSS3, JS
+
+- React, Vue.js and relative ecosystem
+
+- NoSQL DB, Relational DB (Oracle, MySQL, Microsoft SQL)
+
+- C#, RAZOR, MVC framework
+
+- Website performance optimization
+
+- Agile`,
+      },
+    ],
+  },
+  {
+    title: "Volunteer",
+    content: [
+      {
+        title: "Full-stack senior developer",
+        duration: "",
+        link: "",
+        company: "",
+        text: `Organiser Bologna JavaScript Community/Meetup2015 - Today
+        http://bolognajs.com/
+        BolognaJS is a community for JavaScript developers in Bologna. We made workshops, talks, networking meetings, we believe in sharing knowledge and we study new technologies on JavaScript ecosystem. I love to meet people and to speak about Programming feature, technique and do something together.
+        
+        See our last meetup on: https://www.meetup.com/Bologna-JS-Meetup/`,
+      },
+    ],
+  },
+  {
+    title: "Education",
+    content: [
+      {
+        title: "Full-stack senior developer",
+        duration: "",
+        link: "",
+        company: "",
+        text: `Master's degree in Computer Science University of Bologna (UniBo)2005 - 2008
+        Bachelor’s degree in Computer Science University of Bologna (UniBo)2001 - 2005`,
+      },
+    ],
+  }
+];
--- src/pages/md-page.md
+++ src/pages/md-page.md
@@ -5,7 +5,7 @@ setup: |
   import Skills from "../components/Skills.astro";
   import SectionTitle from "../components/SectionTitle.astro";
   import Experience from "../components/Experience.astro";
-  import { data as contents } from "../data/data.js";
+  import { contents as data } from "../data/contents.js";
 title: Edoardo Gargano
 description: Senior front-end developer, full-stack developer, tech lead, JavaScript lover and Bologna JS Community Organizer with more than 10 years of experience in multinational companies
 ---
@@ -14,7 +14,8 @@ description: Senior front-end developer, full-stack developer, tech lead, JavaSc
 <SectionTitle title="Skills">
   <Skills />
 </SectionTitle>
-{contents.map(c => <SectionTitle title={c.title}>
-  <Experience title={c.content.title} duration={c.content.duration} link={c.content.link} company={c.content.company} content={c.content.text} />
+{data.map(c => <SectionTitle title={c.title}>
+  {c.content.map(section => <Experience title={section.title} duration={section.duration} link={section.link} company={section.company} content={section.text} />)}
 </SectionTitle>
 )}
+LANGUAGES TODO COMPONENT
