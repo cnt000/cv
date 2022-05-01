1f1a9666dd4c83d4c4196bb2ce3b5515290c56b2
cnt000 <cnt000@users.noreply.github.com>
2022-05-01 12:04:59 +0000

    deploy: a513fa8fd51a0b3cd8e4b6b11264306116bd869d

---
+++ entry.c0742fd3.js
@@ -0,0 +1,1 @@
+document.body.querySelector(".switch-theme").addEventListener("click",()=>{const e=document.querySelector("html");e.classList.contains("dark")?e.classList.remove("dark"):e.classList.add("dark")});
