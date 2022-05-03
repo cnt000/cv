5a5dcef604f1382abd87202b65ecfc3bb5cad2e7
Edoardo Gargano (cnt000) <cnt000@gmail.com>
2022-05-03 22:05:04 +0200

    gstatic font

--- src/data/header.js
+++ src/data/header.js
@@ -2,8 +2,8 @@ export const header = {
   name: "Edoardo Gargano",
   description:
     "Front-end senior developer, full-stack developer, JavaScript Lover",
-  address: "via A. Bergamini 6, 40133,de Bologna, Italy",
-  mail: "edoardo.gargano [at] gmail.com",
+  address: "via A. Bergamini 6, 40133, Bologna (Italy)",
+  mail: "edoardo.gargano[at]gmail.com",
   number: "+39 329 54 166 26",
   socials: [
     {
